5d1a524e885c149e33451636e177a7ab73ebd13c
AyushMore <ayushmorebb@gmail.com>
2023-09-26 06:49:04 +0530

    devops prac

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello {
 
     public static void main(String[] args) {
-
+// http://downloads.puppetlabs.com/windows/puppet-agent-x64-latest.msi
         // Prints "Hello, World" in the terminal window.
         System.out.println("Hello, World");
     }
